bdc38508abaae1403dc4e4f85de4a4327f1bbcb7
Unknown <allenchou0403@gmail.com>
2016-09-19 22:17:39 +0800

    php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>StarBurstStream</title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body{
+				color: #ffffff;
+				background: #000000;
+			}
+		</style>
+	</head>
+	<body style="">
+	<h1>把神統海名你這一節功臺</h1>
+	<hr/>
+	<p>加相美無星以近<br/>，為電國廣都明變企什舞此人意定</p>
+	<hr/>
+	<a href="./index.php"><img src="./RW.png"/></a>
+	</body>
+</html>
